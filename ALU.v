37ea324b45105f656dc44e3bac9d38c3eb4414ca
Adithya <adishum2002@gmail.com>
2024-03-19 19:34:14 +0530

    ALU,mux4 added slt support

--- ALU.v
+++ ALU.v
@@ -3,14 +3,16 @@
 module ALU(output [31:0] out32,input [31:0] A32,B32, input [2:0] ALUop
 );
 
-wire [31:0] and_out, or_out, add_out, sub_out;
+wire [31:0] and_out, or_out, add_out, sub_out,slt_out,invalid;
 
-//ALU CODES ADD 010, SUB 011, AND 000, OR 001
+//ALU CODES ADD 010, SUB 110, AND 000, OR 001 , SLT 111
 
 assign and_out = A32 & B32;
 assign or_out = A32 | B32;
 assign add_out = A32 + B32;
 assign sub_out = A32 - B32;
+assign slt_out = (A32 < B32) ? 32'h00000001 : 32'b0;
+assign invalid = 32'bx;
 
 wire [1:0] select;
 
@@ -20,7 +22,11 @@ mux4 m4(
     .in1(and_out),
     .in2(or_out),
     .in3(add_out),
-    .in4(sub_out),
-    .sel(ALUop[2:0]));
+    .in4(invalid),
+    .in5(invalid),
+    .in6(invalid),
+    .in7(sub_out),
+    .in8(slt_out),
+    .sel(ALUop));
     
 endmodule
--- mux4.v
+++ mux4.v
@@ -1,12 +1,16 @@
 `timescale 1ns / 1ps
 
 module mux4 (
-    output [31:0] out32, input [31:0] in1, in2, in3, in4, input [2:0] sel
+    output [31:0] out32, input [31:0] in1, in2, in3, in4,in5,in6,in7,in8, input [2:0] sel
     );
 
     assign out32 = (sel == 3'b000) ? in1 : 
-                  (sel == 3'b001) ? in2 : 
-                  (sel == 3'b010) ? in3 : 
-                  (sel == 3'b011) ? in4 : 32'b0;
-    
+                    (sel == 3'b001) ? in2 : 
+                    (sel == 3'b010) ? in3 : 
+                    (sel == 3'b011) ? in4 :
+                    (sel == 3'b100) ? in5 :
+                    (sel == 3'b101) ? in6 :
+                    (sel == 3'b110) ? in7 :
+                    (sel == 3'b111) ? in8 : 32'bx;
+
 endmodule
